57c3f87f9c99ee8db894ca0a482270bcbdd80397
Tor Valamo <tor.valamo@gmail.com>
2010-09-20 06:32:32 +0200

    YAER: Yet another event rewrite. I think it works properly now!

--- simpleaudio.js
+++ simpleaudio.js
@@ -3,253 +3,296 @@
  * http://sam.zoy.org/wtfpl/COPYING
  *
  * For an overview of these functions and what they do, see
- * http://github.com/torvalamo/htmlaudio/wiki
+ * http://github.com/torvalamo/htmlaudio/wiki/Simple-Reference
  */
 
 (function() {
-	var htmlaudio = {};
-	var _path = '';
-	var _sounds = {};
-	var _supported = false;
-	var _volume = 1.0;
+	window.htmlaudio = {}
+	var _sounds = {}
+	var _path = ''
+	var _extension = false
+	var _volume = 1
 	
-	// Check for support.
 	try {
 		var audioObj = new Audio('');
-		if (audioObj.canPlayType('audio/ogg')) _supported = '.ogg';
-		else if (audioObj.canPlayType('audio/mpeg')) _supported = '.mp3';
+		if (audioObj.canPlayType('audio/ogg')) _extension = '.ogg';
+		else if (audioObj.canPlayType('audio/mpeg')) _extension = '.mp3';
 		delete audioObj;
 	} catch(e) {}
 	
-	/**
-	 * Arguments validator.
-	 * bool _args(array args, array types)
-	 * names = [name, name, name, ...]
-	 * types = [type, [type optional], type, ...]
-	 */
+	function _type(value, type) {
+		if (type instanceof RegExp) return type.match(value)
+		else if (type === 'regexp') return value instanceof RegExp
+		else if (type === 'array') return value instanceof Array
+		else return typeof value === type
+	}
+	
 	function _(args, names, types) {
-		var a = 0, out = {};
+		var a = 0, out = {}
 		for (var t in types) {
 			var opt = types[t] instanceof Array,
-				type = opt ? types[t][0] : types[t];
+				type = opt ? types[t][0] : types[t]
 			if (!_type(args[a], type)) { // wrong type
-				if (!opt || (args[a] !== undefined && types.length == 1)) {
-					throw new Error('Wrong argument type (got ' + args[a] + ', expected ' + type + ').'); // required, error
-				} else out[names[t]] = undefined; // optional, skip to next format
-			} else out[names[t]] = args[a++]; // right type, move to next argument
+				if (!opt || (args[a] !== undefined && t == types.length - 1)) {
+					// required or end optional, error
+					throw new Error('Wrong argument type (argument #' + a +
+									' got ' + args[a] + ', expected ' + type +
+									').')
+				} else {
+					// optional, skip to next format
+					out[names[t]] = undefined
+				}
+			} else {
+				// right type, move to next argument
+				out[names[t]] = args[a++]
+			}
 		}
-		return out;
+		return out
 	}
 	
-	function _type(value, type) {
-		if (type instanceof RegExp) return type.match(value);
-		else if (type === 'array') return value instanceof Array;
-		else return typeof value === type;
+	function Sound(name) {
+		this.play = function() {
+			if (!_extension) return
+			if (this.audio.ended) {
+				// Necessary to make browsers emit the 'play' event if the
+				// sound has already played once.
+				this.audio.pause()
+			}
+			this.audio.play()
+		}
+		
+		this.stop = function() {
+			if (!_extension) return
+			if (this.audio.currentTime) this.audio.currentTime = 0
+			this.audio.pause()
+		}
+		
+		this.volume = function(volume) {
+			if (!_extension) return
+			this.audio.volume = volume
+		}
+		
+		this.listeners = {}
+		
+		this.addListener = function(type, eventListener) {
+			if (!this.listeners[type]) this.listeners[type] = []
+			this.listeners[type].push(eventListener)
+		}
+		
+		this.dispatch = function(type) {
+			if (!this.listeners[type]) return
+			for (var f in this.listeners[type]) {
+				this.listeners[type][f](type)
+			}
+		}
+		
+		var that = this
+		this.url = ''
+		
+		if (!_extension) {
+			setTimeout(function() {
+				that.dispatch('loadstart')
+				that.dispatch('loaded')
+			}, 10)
+			return
+		}
+		
+		this.url = _path + name + _extension
+		this.audio = new Audio(this.url)
+		this.audio.volume = _volume
+		
+		this.loaded = function() {
+			that.dispatch('loaded')
+			that.audio.removeEventListener('progress', that.eventDispatcher, false)
+			that.audio.removeEventListener('stalled', that.eventDispatcher, false)
+			delete that.progress
+			delete that.loaded
+		}
+		
+		this.eventDispatcher = function(event) {
+			var type = event.type
+			switch(type) {
+			case 'loadstart':
+				that.audio.removeEventListener('loadstart', that.eventDispatcher, false)
+				break
+			case 'progress':
+				clearTimeout(that.progress)
+				that.progress = setTimeout(that.loaded, 3500)
+				break
+			case 'stalled':
+				clearTimeout(that.progress)
+				return
+			case 'canplaythrough':
+				that.audio.removeEventListener('canplaythrough', that.eventDispatcher, false)
+				break
+			case 'pause':
+				type = 'ended'
+				break
+			case 'ended':
+				that.audio.currentTime = 0 // must be set before pause on FF
+				that.audio.pause()
+				return // don't emit here, see pause
+			}
+			that.dispatch(type)
+		}
+		
+		this.audio.addEventListener('error', this.eventDispatcher, false)
+		this.audio.addEventListener('loadstart', this.eventDispatcher, false)
+		this.audio.addEventListener('progress', this.eventDispatcher, false)
+		this.audio.addEventListener('stalled', this.eventDispatcher, false)
+		this.audio.addEventListener('canplaythrough', this.eventDispatcher, false)
+		this.audio.addEventListener('play', this.eventDispatcher, false)
+		this.audio.addEventListener('pause', this.eventDispatcher, false)
+		this.audio.addEventListener('ended', this.eventDispatcher, false)
+		
+		this.audio.load()
 	}
 	
 	/**
-	 * Internal events handler.
+	 * boolean add(string name)
+	 * boolean add(string name, function eventHandler)
 	 */
-	function _ev(event, name) {
-		switch(event) {
-		case 'error':
-			delete _sounds[name];
-			break;
-		case 'loadstart':
-			if (!_supported) break;
-			// should only happen once so we free some memory
-			_sounds[name].removeEventListener('loadstart', _sounds[name]._lst, false);
-			delete _sounds[name]._lst;
-			break;
-		case 'progress':
-			clearTimeout(_sounds[name]._time_stalled);
-			_sounds[name]._time_stalled = setTimeout(function() {_ev('loaded', name)}, 3500);
-			break;
-		case 'stalled':
-			clearTimeout(_sounds[name]._time_stalled);
-			return;
-		case 'canplaythrough':
-			// happens every time we reset currentTime on FF, so we just cancel
-			// it after the first time
-			_sounds[name].removeEventListener('canplaythrough', _sounds[name]._cpt, false);
-			delete _sounds[name]._cpt;
-			break;
-		case 'loaded':
-			delete _sounds[name]._time_stalled;
-			break;
-		case 'pause':
-			event = 'ended'; // always emit ended when sound stops
-			break;
-		case 'ended':
-			_sounds[name].currentTime = 0; // must be set before pause on FF
-			_sounds[name].pause();
-			return; // don't emit here, see pause
-		}
-		if (_sounds[name].eventHandler) _sounds[name].eventHandler(event, name);
-	}
-	
-	/**
-	 * bool add(string name)
-	 * bool add(string name, function eventHandler)
-	 * Load a sound file.
-	 */
-	function add() {
+	window.htmlaudio.add = function() {
 		with (_(arguments,
 				['name', 'eventHandler'],
 				['string', ['function']])) {
-			if (_sounds[name]) return false;
-			if (!_supported) {
-				_sounds[name] = {_source: ''}
-				if (eventHandler) {
-					eventHandler('loadstart', name);
-					eventHandler('loaded', name);
-				}
-				return true;
+			if (_sounds[name]) return false
+			
+			var sound = new Sound(name)
+			
+			var eventListener = function(event) {
+				eventHandler && eventHandler(event, name)
 			}
 			
-			var url = _path + name + _supported;
-			_sounds[name] = new Audio(url);
-			_sounds[name]._source = url;
-			_sounds[name].volume = _volume;
+			sound.addListener('error', eventListener)
+			sound.addListener('loadstart', eventListener)
+			sound.addListener('progress', eventListener)
+			sound.addListener('canplaythrough', eventListener)
+			sound.addListener('loaded', eventListener)
+			sound.addListener('play', eventListener)
+			sound.addListener('ended', eventListener)
 			
-			if (eventHandler) {
-				_sounds[name].eventHandler = eventHandler;
-				
-				// attach events
-				_sounds[name].addEventListener('error', function() {_ev('error', name);}, false);
-				_sounds[name]._lst = function() {_ev('loadstart', name);};
-				_sounds[name].addEventListener('loadstart', _sounds[name]._lst, false);
-				_sounds[name].addEventListener('progress', function() {_ev('progress', name);}, false);
-				_sounds[name].addEventListener('stalled', function() {_ev('stalled', name);}, false);
-				_sounds[name]._cpt = function() {_ev('canplaythrough', name)}
-				_sounds[name].addEventListener('canplaythrough', _sounds[name]._cpt, false);
-				_sounds[name].addEventListener('play', function() {_ev('play', name);}, false);
-				_sounds[name].addEventListener('pause', function() {_ev('pause', name);}, false);
-				_sounds[name].addEventListener('ended', function() {_ev('ended', name);}, false);
-			}
-			
-			_sounds[name].load();
-			return true;
+			_sounds[name] = sound
+			return true
 		}
 	}
-	htmlaudio.add = add;
+	
+	/**
+	 * array list()
+	 * array list(string search)
+	 * array list(regexp filter)
+	 * array list(string search, regexp filter)
+	 */
+	window.htmlaudio.list = function() {
+		with (_(arguments,
+				['search', 'filter'],
+				[['string'], ['regexp']])) {
+			var result = []
+			for (var s in _sounds) {
+				if (search && s.indexOf(search) == -1) continue
+				else if (filter && !s.match(filter)) continue
+				result.push(s)
+			}
+			return result
+		}
+	}
 	
 	/**
 	 * string path()
 	 * void path(string path)
-	 * Get or set the path to the folder containing the sound files.
 	 */
-	function path() {
+	window.htmlaudio.path = function() {
 		with (_(arguments,
 				['path'],
 				[['string']])) {
-			if (path === undefined) return _path;
-			if (path !== '' && !path.match(/\/$/)) path += '/';
-			_path = path;
-			return null;
+			if (path == undefined) return _path
+			if (path != '' && !path.match(/\/$/)) path += '/'
+			_path = path
+			return null
 		}
 	}
-	htmlaudio.path = path;
 	
 	/**
-	 * bool play(string name)
-	 * Play the sound identified by name.
+	 * boolean play(string sound)
 	 */
-	function play() {
+	window.htmlaudio.play = function() {
 		with (_(arguments,
-				['name'],
+				['sound'],
 				['string'])) {
-			if (!_sounds[name]) return false;
-			if (!_supported) return true;
-			if (_sounds[name].ended) _sounds[name].pause(); // Hack to force browsers to emit the 'play' event on subsequent plays.
-			_sounds[name].play();
-			return true;
+			if (!_sounds[sound]) return false
+			_sounds[sound].play()
+			return true
 		}
 	}
-	htmlaudio.play = play;
 	
 	/**
 	 * void remove()
-	 * void remove(string name)
-	 * Remove all sounds or just the sound identified by name from the library.
+	 * void remove(string sound)
 	 */
-	function remove() {
+	window.htmlaudio.remove = function() {
 		with (_(arguments,
-				['name'],
+				['sound'],
 				[['string']])) {
-			if (name === undefined) {
-				this.stop();
-				_sounds = {};
-			} else if (_sounds[name]) {
-				this.stop(name);
-				delete _sounds[name];
+			if (sound == undefined) {
+				this.stop()
+				_sounds = {}
+			} else if (_sounds[sound]) {
+				this.stop(sound)
+				delete _sounds[sound]
 			}
-			return null;
+			return null
 		}
 	}
-	htmlaudio.remove = remove;
 	
 	/**
-	 * string source(string name)
-	 * Return the full url of the audio object.
+	 * string source(string sound)
 	 */
-	function source() {
+	window.htmlaudio.source = function() {
 		with (_(arguments,
-				['name'],
+				['sound'],
 				['string'])) {
-			if (!_sounds[name]) return '';
-			return _sounds[name]._source;
+			if (!_sounds[sound]) return ''
+			return _sounds[sound].url
 		}
 	}
-	htmlaudio.source = source;
 	
 	/**
 	 * void stop()
-	 * void stop(string name)
-	 * Stop all playing sounds or just the sound given by name.
+	 * void stop(string sound)
 	 */
-	function stop() {
+	window.htmlaudio.stop = function() {
 		with (_(arguments,
-				['name'],
+				['sound'],
 				[['string']])) {
-			if (name == undefined) {
-				for (var s in _sounds) stop(s);
-				return null;
+			if (sound == undefined) {
+				for (var s in _sounds) _sounds[s].stop()
+			} else if (_sounds[sound]) {
+				_sounds[sound].stop()
 			}
-			if (!_supported || !_sounds[name]) return null;
-			if (_sounds[name].currentTime) _sounds[name].currentTime = 0;
-			_sounds[name].pause();
-			return null;
+			return null
 		}
 	}
-	htmlaudio.stop = stop;
 	
 	/**
-	 * bool supported()
-	 * Tell whether or not the browser can play ogg or mp3 files.
+	 * boolean supported()
 	 */
-	function supported() {
-		return (_supported !== false);
+	window.htmlaudio.supported = function() {
+		return (_extension !== false)
 	}
-	htmlaudio.supported = supported;
 	
 	/**
-	 * float volume()
-	 * void volume(float volume)
-	 * Get or set the volume for all the sounds.
+	 * double volume()
+	 * void volume(double volume)
 	 */
-	function volume() {
+	window.htmlaudio.volume = function() {
 		with (_(arguments,
 				['volume'],
 				[['number']])) {
-			if (!volume) return _volume;
-			if (volume < 0 || volume > 1) volume = 1.0;
-			_volume = volume;
-			for (var s in _sounds) _sounds[s].volume = volume;
-			return null;
+			if (volume == undefined) return _volume
+			if (volume < 0 || volume > 1) volume = 1
+			_volume = volume
+			for (var s in _sounds) _sounds[s].volume(volume)
+			return null
 		}
 	}
-	htmlaudio.volume = volume;
-	
-	window.htmlaudio = htmlaudio;
-}());
+})()
--- tests/simple/add.js
+++ tests/simple/add.js
@@ -4,10 +4,11 @@ test('.add() correct input', function(assert) {
 	assert(htmlaudio.add('heal') === false, 'Add did not return false when adding a sound that already existed.');
 	assert(htmlaudio.source('heal') !== '', 'Audio source URL was not what was expected.');
 	assert(htmlaudio.source('heal').match(/\/sounds\/heal\.(mp3|ogg)$/), 'Audio source URL was not what was expected.');
+	assert(htmlaudio.add('sound', function() {}) === true, 'Add did not return true when successfully adding sound with event handler.');
 });
 
 test('.add() erroneous input', function(assert) {
-	try {assert(htmlaudio.add() === null, 'Add complained when not supplying arguments. Should throw exception.');}
+	try {htmlaudio.add();assert(false, 'Add complained when not supplying arguments. Should throw exception.');}
 	catch(e) {assert(e instanceof Error, 'Add did not throw a proper exception.');}
 	try {htmlaudio.add(null);assert(false, 'Add complained when adding invalid type name (null). Should throw exception.');}
 	catch(e) {assert(e instanceof Error, 'Add did not throw a proper exception.');}
@@ -16,5 +17,9 @@ test('.add() erroneous input', function(assert) {
 	try {htmlaudio.add(false);assert(false, 'Add complained when adding invalid type name (boolean). Should throw exception.');}
 	catch(e) {assert(e instanceof Error, 'Add did not throw a proper exception.');}
 	try {htmlaudio.add([]);assert(false, 'Add complained when adding invalid type name (array). Should throw exception.');}
+	catch(e) {assert(e instanceof Error, 'Add did not throw a proper exception.');}
+	try {htmlaudio.add('sound', null);assert(false, 'Add complained when adding invalid type event handler (null). Should throw exception.');}
+	catch(e) {assert(e instanceof Error, 'Add did not throw a proper exception.');}
+	try {htmlaudio.add('sound', 123);assert(false, 'Add complained when adding invalid type event handler (number). Should throw exception.');}
 	catch(e) {assert(e instanceof Error, 'Add did not throw a proper exception.');}
 });
