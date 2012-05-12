776600a0272317b2311d6ff643ed30d117251799
Tor Valamo <tor.valamo@gmail.com>
2012-05-12 04:08:53 +0200

    removed unnecessary files

--- tests/simple/supported.js
+++
@@ -1,7 +0,0 @@
-test('.supported() correct input', function(assert) {
-	assert(typeof htmlaudio.supported() === 'boolean', 'supported() should return boolean.');
-});
-
-test('.supported() erroneous input', function(assert) {
-	assert(typeof htmlaudio.supported('something') === 'boolean', 'supported() should return boolean always.');
-});
